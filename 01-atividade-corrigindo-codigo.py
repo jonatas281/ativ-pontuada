e5ebdd01b8e34c24f324306e94d92705bb006458
jonatas281 <jf238330@gmail.com>
2024-09-16 22:06:23 +0000

    atividadepontuada2

--- 01-atividade-corrigindo-codigo.py
+++ 01-atividade-corrigindo-codigo.py
@@ -12,14 +12,10 @@ os . system ("cls || clear" )
 """
 # Variáveis para armazenar as estatísticas
 lista_numero=[]
-quantidade_pares=0
-quantidade_impares=0
-quantidade_positivo_negativo=0
+quantidade_positivo_negativo=[]
 maior_numero = 0
 menor_numero = 0
-soma_pares = 0
-soma_impares = 0
-soma_geral = 0
+
 quantidade_numero=5
 
 # Variáveis para armazenar os números
@@ -30,34 +26,23 @@ for i in range(quantidade_numero):
 
 #processando cada numero
 def verificar_pares_impares(numero):
-    if numero %2 == 0:
-        quantidade_pares +=1
-    else:
-        quantidade_impares +=1
-    return quantidade_pares,quantidade_impares
-
-
-
-
-
+    qtd_pares=0
+    qtd_impares=0
+    for numero in numero:
+        if numero % 2 == 0:
+            qtd_pares +=1
+        else:
+            qtd_impares +=1
+   
 
 # Calculando as médias
-media_pares = soma_pares / quantidade_pares if quantidade_pares > 0 else 0
-media_impares = soma_impares / quantidade_impares if quantidade_impares > 0 else 0
-media_geral = soma_geral / 5
 
 # Mostrando números na ordem inversa
+pares,impares=verificar_pares_impares(lista_numero)
+for i in range (quantidade_numero):
+    numero=int(input(f"digite o {i+1}ª: numero "))
+
+print(f"quantidade de pares: {quantidade_pares} ")
+print(f"quantidadede impares: {quantidade_impares} ")
 
 
-# Imprimindo as estatísticas
-print("\nEstatísticas dos números:")
-print(f"Quantidade de pares: {quantidade_pares}")
-print(f"Quantidade de ímpares: {quantidade_impares}")
-print(f"Quantidade de positivos: {quantidade_positivos}")
-print(f"Quantidade de negativos: {quantidade_negativos}")
-print(f"Maior número: {maior_numero}")
-print(f"Menor número: {menor_numero}")
-print(f"Média dos números pares: {media_pares:.2f}")
-print(f"Média dos números ímpares: {media_impares:.2f}")
-print(f"Média de todos os números: {media_geral:.2f}")
-print(f"Números na ordem inversa: {numeros_invertidos}")
